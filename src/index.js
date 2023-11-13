d83743b947b78630c64d63a6c09b5634ed7d175f
maliha shahi <maliha.shahi.03@gmail.com>
2023-11-13 22:04:21 +0330

    timer simple project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
+
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+const tick =()=>{
+    const elem=(
+      <div>
+        <h1>Hello Gyus</h1>
+        <h2>it is
+          {
+          new Date().toLocaleTimeString()  
+          }
+        </h2>
+      </div>
+    )
+    ReactDOM.render(elem, document.getElementById('root'));
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  }
+  setInterval(() => {
+    tick();
+  }, 1000);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+
 reportWebVitals();
