87183bfa57022b22063113a5ac624674cb4a2f4e
maliha shahi <maliha.shahi.03@gmail.com>
2023-11-22 23:08:01 +0330

    first hook react  project 'stop watch'

--- src/app2.js
+++ src/app2.js
@@ -1,33 +1,50 @@
-import React, { Component } from 'react';
+
+import React, { useState} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './style.css';
 import './style.css';
 import Hello from './hello';
 import Timer from './timer';
 
 
-class App extends React.Component {
-  constructor(){
-   super();
-   this.state={
-    title :"Hi firends"
-   } 
-  }
-handleSetTitle =()=>{
-  this.setState({
-    title: "welcome to my website"
-  })
-}
+// class App extends React.Component {
+//   constructor(){
+//    super();
+//    this.state={
+//     title :"Hi firends"
+//    } 
+//   }
+// handleSetTitle =()=>{
+//   this.setState({
+    //title: "welcome to my website"
+//   })
+// }
 
-  render(){
-    return(
-    <div className='main'>
-    <Hello title={this.state.title}/>
-    <Timer handleSetTitle={this.handleSetTitle}/>
+
+//   render(){
+//     return(
+//     <div className='main'>
+//     <Hello title={this.state.title}/>
+//     <Timer handleSetTitle={this.handleSetTitle}/>
  
-  </div>
-  )
-  }
+//   </div>
+//   )
+//   }
 
+// }
+const App =()=>{
+
+  const [ title, setTitle]=useState("Hi firends");
+  const handleSetTitle= ()=>{
+    setTitle("hello firends")
+  }
+  return(
+        <div className='main'>
+        <Hello title={title}/>
+        <Timer handleSetTitle={handleSetTitle}/>
+     
+      </div>
+      )
 }
 export default App;
